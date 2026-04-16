0c30097a0f8f3cf1424b1d12db398464c27432ff
Aaron <406505223@qq.com>
2026-04-16 15:19:20 +0800

    Mercury Mac开关 和 sigmob AndroidID开关控制逻辑修复

--- advancesigmob/src/main/java/com/advance/supplier/sigmob/SigmobUtil.java
+++ advancesigmob/src/main/java/com/advance/supplier/sigmob/SigmobUtil.java
@@ -163,6 +163,9 @@ public class SigmobUtil {
                      */
                     @Override
                     public boolean isCanUseAndroidId() {
+                        if (!advancePrivacyController.isCanUsePhoneState()) {
+                            return false;
+                        }
                         String cusAID = advancePrivacyController.getDevAndroidID();
                         //如果用户传递为空，或默认未配置，true，允许SDK获取； 若用户配置了id，则不再允许SDK获取
                         return BYStringUtil.isEmpty(cusAID);
