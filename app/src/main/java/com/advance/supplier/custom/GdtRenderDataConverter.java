22ecd2eecaf79c2777b9092dbeeca839bbd83c97
Aaron <406505223@qq.com>
2026-07-07 15:31:53 +0800

    v5.2.7.1 更新

--- app/src/main/java/com/advance/supplier/custom/GdtRenderDataConverter.java
+++ app/src/main/java/com/advance/supplier/custom/GdtRenderDataConverter.java
@@ -52,19 +52,7 @@ public class GdtRenderDataConverter implements AdvanceRFADData {
 
     }
 
-    @Override
-    public AdvanceSdkSupplier getSdkSupplier() {
-        AdvanceSdkSupplier advanceSdkSupplier = new AdvanceSdkSupplier();
-        try {
-            if (mSdkSupplier != null) {
-                advanceSdkSupplier.adnId = mSdkSupplier.id;
-                advanceSdkSupplier.adspotId = mSdkSupplier.adspotid;
-            }
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-        return advanceSdkSupplier;
-    }
+
 
     @Override
     public String getTitle() {
